1d86ed62583de7f00d6ca14af9c595dd58d9bd45
dairsaber <dairsaber@outlook.com>
2022-07-23 11:58:31 +0800

    feat: 集成iconify

---
+++ src/components/Icon/src/MyIcon.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup name="MyIcon">
+  import { Icon } from "@iconify/vue"
+  import { computed } from "vue"
+  type BasicProps = {
+    icon: string
+    size?: number | string
+    spin?: boolean
+  }
+  const props = withDefaults(defineProps<BasicProps>(), { size: 16, spin: false })
+
+  const getIconClass = computed(() => {
+    if (props.spin !== false) {
+      return "_spin"
+    }
+    return ""
+  })
+</script>
+
+<template>
+  <Icon class="_myicon" :class="getIconClass" :icon="icon" :height="size" />
+</template>
+
+<style lang="less" scoped>
+  ._myicon {
+    &._spin {
+      animation: loadingCircle 1s infinite linear;
+    }
+  }
+
+  @keyframes loadingCircle {
+    0% {
+      transform: rotateZ(0deg);
+    }
+    100% {
+      transform: rotateZ(360deg);
+    }
+  }
+</style>
--- src/pages/examples/test/IconTest.vue
+++ src/pages/examples/test/IconTest.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup name="IconTest">
-  import { Icon } from "@iconify/vue"
+  import Icon from "@/components/Icon"
 </script>
 
 <template>
   <div class="flex items-center justify-center w-full h-full text-5xl">
-    <Icon icon="mdi:home" />
-    <Icon icon="mdi:account" />
+    <Icon icon="mdi:home" size="32" style="color: red" />
+    <Icon icon="mdi:account" spin />
     <Icon icon="ant-design:account-book-filled" />
   </div>
 </template>
