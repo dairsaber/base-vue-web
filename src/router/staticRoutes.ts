d4cfbc50e94da8ac53011da16211c7b09beb0d22
dairsaber <dairsaber@outlook.com>
2022-07-23 12:27:22 +0800

    fix: 解决build报错的问题

--- src/router/staticRoutes.ts
+++ src/router/staticRoutes.ts
@@ -3,8 +3,9 @@ const modules = import.meta.globEager("./modules/**/*.ts")
 /** 一张图上面的页面路由 */
 const routeModuleList: RouteRecordRaw[] = []
 
-Object.keys(modules).forEach((key) => {
-  const mod = modules[key].default || {}
+Object.keys(modules).forEach((key: string) => {
+  const mod = (modules[key] as Recordable).default ?? {}
+
   const modList = Array.isArray(mod) ? [...mod] : [mod]
   routeModuleList.push(...modList)
 })
